3454e9548e0c9271f07ee4af0babedcc1412749c
okeyLiu <okeyLiu@users.noreply.github.com>
2019-07-16 16:56:46 +0800

    IndexController注释修改3
    IndexController注释修改3

--- src/main/java/com/feifan/controller/IndexController.java
+++ src/main/java/com/feifan/controller/IndexController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 /*
 这是IndexController的作用是测试jsp的支持和路径是否ok
 SpringBoot是可以跑JSP的
+写的什么注释，还好意思发
 */
 @Controller
 public class IndexController {
