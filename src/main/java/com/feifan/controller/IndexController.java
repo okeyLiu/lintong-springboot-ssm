c77cc5a13bf647fec44559ed8afca6b1401620a3
liuhaolie <okeyliu@163.com>
2019-07-16 16:59:26 +0800

    开骂，来战！！！

--- src/main/java/com/feifan/controller/IndexController.java
+++ src/main/java/com/feifan/controller/IndexController.java
@@ -7,10 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 这是IndexController的作用是测试jsp的支持和路径是否ok
 SpringBoot是可以跑JSP的
 写的什么注释，还好意思发
+来，开骂！！who pa who !!!
 */
 @Controller
 public class IndexController {
     @RequestMapping("/index")
+    //知道这是什么吗
     public String index() {
         return "index";
     }
