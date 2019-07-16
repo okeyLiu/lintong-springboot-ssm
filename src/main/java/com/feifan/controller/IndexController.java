b3786982f91ca92198d4dd53755316a057c42ed0
okeyLiu <okeyLiu@users.noreply.github.com>
2019-07-16 16:51:39 +0800

    增加IndexController的注释
    增加IndexController的注释

--- src/main/java/com/feifan/controller/IndexController.java
+++ src/main/java/com/feifan/controller/IndexController.java
@@ -3,6 +3,9 @@ package com.feifan.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/*
+这是IndexController的作用是测试jsp的支持和路径是否ok
+*/
 @Controller
 public class IndexController {
     @RequestMapping("/index")
