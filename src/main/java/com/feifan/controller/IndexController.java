24f03f5fea2a14a53ff9d46873b991db7d22bd3d
liuhaolie <okeyliu@163.com>
2019-07-16 16:52:47 +0800

    增加IndexController的类上的注释

--- src/main/java/com/feifan/controller/IndexController.java
+++ src/main/java/com/feifan/controller/IndexController.java
@@ -3,6 +3,9 @@ package com.feifan.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 这是测试Jsp是否被支持！！
+ */
 @Controller
 public class IndexController {
     @RequestMapping("/index")
